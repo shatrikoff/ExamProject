246fc5a997a6a151e2d6f4a115c08037d183406c
shatrikoff <78554701+shatrikoff@users.noreply.github.com>
2021-08-08 13:39:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "webserver" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "${var.instance_type}"
-  key_name        = "${var.key_name}"
+  #key_name        = "${var.key_name}"
   vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
   user_data       = "${file("userdata.sh")}"
   lifecycle {
